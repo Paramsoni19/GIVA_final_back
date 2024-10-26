22bfd26deceed461df3d23742ca28ef7a6ba1767
Param Soni <paramsoni@Params-Laptop.local>
2024-10-26 18:57:57 +0530

    add database values

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "kth18822",
-    host: "localhost",
+    user: "giva_final_database_user",
+    password: "BoAQwsJMtfjDFZTgBqSuQKw7963g5QSa",
+    host: "dpg-cseeonu8ii6s7392e760-a.oregon-postgres.render.com",
     port: 5432,
-    database: "perntodo"
+    database: "giva_final_database"
 });
 
 module.exports = pool;
